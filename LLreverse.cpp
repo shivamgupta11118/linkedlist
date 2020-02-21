3505d64e6245ad964c17f3adf7ec75418b2fb311
shivamgupta11118 <61278967+shivamgupta11118@users.noreply.github.com>
2020-02-21 16:45:25 +0530

    reverse of linkedlist

---
+++ LLreverse.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+class node{
+public:
+int data;
+node* Next;
+node(int d){
+    data=d;
+    Next=NULL;
+}
+};
+ void insertathead(node*&head,int data){
+node* n=new node(data);
+n->Next=head;
+head=n;
+}
+void insertattail(node*&head,int data){
+   if(head==NULL){
+     //  insertathead(head,data);
+     head=new node(data);
+     return;
+   }
+
+    node* tail=head;
+    node* n=new node(data);
+    while(tail->Next!=NULL){
+      tail=tail->Next;
+    }
+    tail->Next=n;
+}
+
+
+       
+     
+
+  
+void buildlist(node*& head){
+    int data;
+    cin>>data;
+    while(data!=-1){
+        insertattail(head,data);
+        cin>>data;
+    }
+}
+istream& operator>>(istream &is,node* &head){
+    buildlist(head);
+    return is;
+}
+ostream& operator<<(ostream &os,node* head){
+    print(head);
+    return os;
+}
+void reverse(node *&head){
+    node*prev=NULL;
+    node*temp=head;
+    node*n;
+    while(temp!=NULL){
+        n=temp->Next;
+        temp->Next=prev;
+        prev=temp;
+       temp=n;
+        //temp=temp->Next;
+    }
+    head=prev;
+}
+
+void print(node*head){
+    node*temp=head;
+    while(head!=NULL){
+        cout<<head->data<<"-->";
+        head=head->Next;
+    }
+    cout<<endl;
+}
+
+int main() {
+    node*head=NULL;
+
+
+cin>>head;
+cout<<head;
+reverse(head);
+cout<<head;
+return 0;
+
+}
