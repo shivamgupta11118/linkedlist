ce03b88b059b366a229b792c9f9017594fcabbb7
shivamgupta11118 <61278967+shivamgupta11118@users.noreply.github.com>
2020-02-21 16:49:19 +0530

    reverse using recurrsion

---
+++ LLreverserecurrsion.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+class node{
+public:
+int data;
+node* Next;
+node(int d){
+    data=d;
+    Next=NULL;
+}
+};
+ void insertathead(node*&head,int data){
+node* n=new node(data);
+n->Next=head;
+head=n;
+}
+void insertattail(node*&head,int data){
+   if(head==NULL){
+     //  insertathead(head,data);
+     head=new node(data);
+     return;
+   }
+
+    node* tail=head;
+    node* n=new node(data);
+    while(tail->Next!=NULL){
+      tail=tail->Next;
+    }
+    tail->Next=n;
+}
+
+
+       
+     
+
+  
+void buildlist(node*& head){
+    int data;
+    cin>>data;
+    while(data!=-1){
+        insertattail(head,data);
+        cin>>data;
+    }
+}
+istream& operator>>(istream &is,node* &head){
+    buildlist(head);
+    return is;
+}
+ostream& operator<<(ostream &os,node* head){
+    print(head);
+    return os;
+}
+
+node* reverserec(node* head){
+    if(head->Next==NULL || head==NULL){
+        return head;
+    }
+    node* sh=reverserec(head->Next);
+    node*c=head;
+    c->Next->Next=c;
+    c->Next=NULL;
+    return sh;
+}
+void print(node*head){
+    node*temp=head;
+    while(head!=NULL){
+        cout<<head->data<<"-->";
+        head=head->Next;
+    }
+    cout<<endl;
+}
+
+int main() {
+    node*head=NULL;
+
+
+cin>>head;
+cout<<head;
+head=reverserec(head);
+cout<<head;
+return 0;
+
+}
