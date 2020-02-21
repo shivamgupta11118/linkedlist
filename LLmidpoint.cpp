8dcc1fd7f902bd22d987d90706a55d6aebf8b5ce
shivamgupta11118 <61278967+shivamgupta11118@users.noreply.github.com>
2020-02-21 17:04:22 +0530

    midpoint of linkedlist using midpoint runner technique

---
+++ LLmidpoint.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+class node{
+public:
+int data;
+node* Next;
+node(int d){
+    data=d;
+    Next=NULL;
+}
+};
+ void insertathead(node*&head,int data){
+node* n=new node(data);
+n->Next=head;
+head=n;
+}
+void insertattail(node*&head,int data){
+   if(head==NULL){
+     //  insertathead(head,data);
+     head=new node(data);
+     return;
+   }
+
+    node* tail=head;
+    node* n=new node(data);
+    while(tail->Next!=NULL){
+      tail=tail->Next;
+    }
+    tail->Next=n;
+}
+
+
+       
+     
+
+  
+void buildlist(node*& head){
+    int data;
+    cin>>data;
+    while(data!=-1){
+        insertattail(head,data);
+        cin>>data;
+    }
+}
+istream& operator>>(istream &is,node* &head){
+    buildlist(head);
+    return is;
+}
+ostream& operator<<(ostream &os,node* head){
+    print(head);
+    return os;
+}
+node* midpoint(node* head){
+    if(head==NULL || head->Next==NULL){
+        return head;
+    }
+    node* slow=head;
+    node* fast=head->Next;
+    while(fast!=NULL && fast->Next!=NULL){
+        slow=slow->Next;
+        fast=fast->Next->Next;
+    }
+    return slow;
+}
+
+void print(node*head){
+    node*temp=head;
+    while(head!=NULL){
+        cout<<head->data<<"-->";
+        head=head->Next;
+    }
+    cout<<endl;
+}
+
+int main() {
+    node*head=NULL;
+
+
+cin>>head;
+cout<<head;
+node *m=midpoint(head);
+cout<<m->data<<endl;
+return 0;
+
+}
